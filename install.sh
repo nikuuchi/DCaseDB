c957820738a0cfc17e13a0b2f66fd5fc3e69a4b0
uchida <uchida.atsushi.8@gmail.com>
2013-02-11 22:22:02 +0900

    refactoring

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo mkdir -p /usr/local/lib/konoha/0.3/package/Deos.DCaseCloud
-sudo cp DCaseCloud_glue.k /usr/local/lib/konoha/0.3/package/Deos.DCaseCloud
+sudo mkdir -p /usr/local/lib/konoha/3.0/package/Deos.DCaseCloud
+sudo cp DCaseCloud_glue.k /usr/local/lib/konoha/3.0/package/Deos.DCaseCloud
